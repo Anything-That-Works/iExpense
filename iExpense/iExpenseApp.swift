1a65d3f86688d7b0661de5289d73792e3b9b0821
Promal B <promal.barua.cse@gmail.com>
2021-07-26 01:28:46 +0600

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Promal on 26/7/21.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
